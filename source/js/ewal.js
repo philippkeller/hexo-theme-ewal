02ab9e5df1e0189491f7068b2ec0bff7b595d8af
Philipp Keller <philipp.keller@gmail.com>
2015-09-12 13:42:25 +0200

    no sidebar when in page layout, fixed search field, added previous/next buttons

--- source/js/ewal.js
+++ source/js/ewal.js
@@ -7,3 +7,12 @@ $(window).load(function() {
 	});
 });
 
+;(function( $ ) {
+    $(function() {
+        $( '[data-js="google-search-form"]' ).on( 'submit', function( e ) {
+            e.preventDefault();
+            var inputVal = $( '[data-js="google-search-input"]' ).val();
+            window.location.href = $( this ).attr( 'data-action' ) + ' ' + inputVal;
+        });
+    });
+})( jQuery );
